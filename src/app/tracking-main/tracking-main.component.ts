e744449f125aa9927d40aae2cae41869556a1259
Grzegorz <gszumilas92@gmail.com>
2017-08-01 19:18:43 +0200

    Setting up Project

---
+++ src/app/tracking-main/tracking-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tracking-main',
+  templateUrl: './tracking-main.component.html',
+  styleUrls: ['./tracking-main.component.css']
+})
+export class TrackingMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
